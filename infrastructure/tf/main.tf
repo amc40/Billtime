8e7c5882c30d1856eae85fc3d0161711a80a2ab2
amc40 <36701168+amc40@users.noreply.github.com>
2023-06-17 16:31:06 +0100

    Create Initial Terraform + k8s config (#1)
    * WIP: initial terraform
    
    * WIP: provision digital ocean resources
    
    * droplet and managed db in tf
    
    * WIP: move to k8s cluster
    
    * add doctl to dev container
    
    * ignore k8s secrets
    
    * move to deployment per node
    
    * add networking rules
    
    * fixes to tf
    
    * move infrastructure into new top-level folder
    
    * update server to match the infrastructure
    
    ---------
    
    Co-authored-by: Alan Chodyniecki <alan.chodyniecki@softwire.com>
    Co-authored-by: Alan <alan.chodygmail.com>

---
+++ infrastructure/tf/main.tf
@@ -0,0 +1,62 @@
+  variable "do_token" {
+   type = string
+    description = "Digital Ocean API token"
+}
+
+locals {
+  environment = terraform.workspace
+  location = "lon1"
+  common_tags = ["terraform", "BillTime", local.environment]
+}
+
+
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+  backend "s3" {
+    bucket         	   = "billtime-terraform-backend"
+    key              	   = "state/terraform.tfstate"
+    region         	   = "eu-west-2"
+    encrypt        	   = true
+    dynamodb_table = "billtime-terraform-lockid"
+  }
+}
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_project" "billtime_project" {
+  name        = "billtime-${local.environment}"
+  description = "A ${local.environment} environment for the billtime app."
+  purpose     = "Web Application"
+  environment = title(local.environment)
+  resources = [
+    digitalocean_kubernetes_cluster.billtime_k8s_cluster.urn,
+  ]
+}
+
+resource "digitalocean_container_registry" "webb_and_wallace_container_registry" {
+  name                   = "webb-and-wallace-container-registry"
+  subscription_tier_slug = "starter"
+  # No option to deploy in lon1 region
+  region    = "ams3"
+}
+
+resource "digitalocean_kubernetes_cluster" "billtime_k8s_cluster" {
+  name   = "billtime-k8s-cluster"
+  region = local.location
+  version = "1.27.2-do.0"
+  node_pool {
+    name       = "worker-pool"
+    size       = "s-1vcpu-2gb"
+    node_count = 2
+    tags = local.common_tags
+  }
+  tags = local.common_tags
+}
